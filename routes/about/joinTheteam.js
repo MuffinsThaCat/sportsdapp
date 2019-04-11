712009922d6cc54e4466ca1b3c623f9c2a68b618
murgor <murgornaftali@gmail.com>
2019-04-11 12:34:41 +0300

    Typo Fixes and code refactoring

--- routes/about/joinTheteam.js
+++ routes/about/joinTheteam.js
@@ -2,8 +2,8 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 /* GET join the team. */
-router.get('/join-the-team', function(req, res, next) {
-    res.sendFile(path.resolve('views/joint-the-team.html'));
+router.get('/', function(req, res, next) {
+    res.sendFile(path.resolve('views/join-the-team.html'));
 });
 
 module.exports = router;
