c4d1075001d8564f49324327ad97a550194ecc00
murgor <murgornaftali@gmail.com>
2019-04-14 20:44:05 +0300

    Add Isometrics|Initial commits

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,19 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 
-/* GET copany page. */
+/* GET index page. */
 router.get('/', function(req, res, next) {
     res.render('index.hbs', {
         layout: false
     });
 });
+
+router.get('/contact', function(req, res, next) {
+    res.render('contact.hbs', {
+        layout: false
+    });
+});
+
 
 
 
