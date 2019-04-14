5db63c77406b5496cf9c03622fcaf854b51c3404
murgor <murgornaftali@gmail.com>
2019-04-15 00:43:38 +0300

    ADD CODE REFACTOR

---
+++ public/js/animate.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+    $(".owl-carousel").owlCarousel({
+
+        autoPlay: 2000, //Set AutoPlay to 2 seconds
+        loop: true,
+        nav: true,
+
+        items: 3,
+        itemsDesktop: [1199, 3],
+        itemsDesktopSmall: [979, 3]
+
+    });
+
+});
